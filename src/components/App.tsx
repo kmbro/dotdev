4108bc16ee6210c5f18e5b7a18671fc48e75a4f4
Kyle Brown <kmbro.aws@gmail.com>
2021-12-24 19:41:07 -0700

    Set up simple home page with typewriter effect and update project settings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,31 @@
-import Avatar from 'components/Avatar'
+import './app.css';
 
 function App() {
   return (
-    <div className="bg-white">
-      <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
-        <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
-            reactjs-vite-tailwindcss-boilerplate
-          </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
+    <div className="overflow-x-hidden overflow-y-scroll w-screen h-screen font-mono bg-white snap-y snap-mandatory dark:bg-slate-700 dark:text-slate-200">
+      <div className="flex flex-col justify-center items-center w-screen h-screen snap-start">
+        <div className="typewriter">
+          <h1 className="pb-1 text-5xl">Hi, I am Kyle</h1>
+        </div>
+        <span className="mt-6 text-xl motion-safe:animate-bounce text-slate-300 dark:text-slate-400">Scroll</span>
+      </div>
+      <div className="flex p-4 w-screen h-screen snap-start bg-orange-300 dark:bg-sky-900">
+        <div className="typewriter">
+          <h1 className="pb-1 text-5xl">Page 2</h1>
+        </div>
+      </div>
+      <div className="flex p-4 w-screen h-screen snap-start">
+        <div className="typewriter">
+          <h1 className="pb-1 text-5xl">Page 3</h1>
+        </div>
+      </div>
+      <div className="flex p-4 w-screen h-screen snap-start bg-orange-300 dark:bg-sky-900">
+        <div className="typewriter">
+          <h1 className="pb-1 text-5xl">Page 4</h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
